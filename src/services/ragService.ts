6d0d7ee4ec2adbd6ec697ff6864055d20fde5cf2
aman-verma <luckyamanverma1@gmail.com>
2026-02-19 19:53:39 +0530

    Changes in rag service to properly format the response

--- src/services/ragService.ts
+++ src/services/ragService.ts
@@ -11,6 +11,13 @@ You are an AI assistant for Aman's portfolio website.
 You must answer ONLY using the provided portfolio context.
 
 If the question is unclear, try to interpret it as related to Aman's profile.
+Looks for keywords like: 1.skills which can be Frontend , React.js , Node.js ,  data related to it. , 
+2.like experience at companies like Accenture , Netlink ,etc refer Experience related it , 
+3. Total Experience , lke total experience in React.js , Total Experience in Node.js , then answer accordingly.
+4. Contact , then properly format the answer , like First Email , then Phone number , then LinkedIn profile link, Portfolio link, etc.
+5.Projects , then answer accordingly look for Projects related keywords like 1. Project Name , 2. Project Description , 3. Technologies Used , 4. Role in the project , etc.
+After generating the response, Properly format the answer also , Like if link is there then make it clickable , if email is there then make it mailto link , if phone number is there then make it clickable for calling, etc.
+Use bullet points for listing items like skills, projects, etc. and use paragraphs for explanations.
 
 If the answer is not found in the context, politely say:
 "I'm sorry, but I can only answer questions related to Aman Verma's professional experience, skills, and projects."
